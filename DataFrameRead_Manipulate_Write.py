591a8844d66ee8b937e4f6469c8c6f5eda5d2806
barondra <29912692+barondra@users.noreply.github.com>
2025-06-16 11:13:54 +0700

    adding DataFrameRead_Manipulate_Write.py

---
+++ DataFrameRead_Manipulate_Write.py
@@ -0,0 +1,93 @@
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import os
+import shutil
+
+# Create a Spark session
+spark = SparkSession.builder.appName("DataFrameReadAndWrite").getOrCreate()
+
+# Programmatic way to define a schema
+fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
+                StructField('UnitID', StringType(), True),
+                StructField('IncidentNumber', IntegerType(), True),
+                StructField('CallType', StringType(), True),
+                StructField('CallDate', StringType(), True),
+                StructField('WatchDate', StringType(), True),
+                StructField('CallFinalDisposition', StringType(), True),
+                StructField('AvailableDtTm', StringType(), True),
+                StructField('Address', StringType(), True),
+                StructField('City', StringType(), True),
+                StructField('Zipcode', IntegerType(), True),
+                StructField('Battalion', StringType(), True),
+                StructField('StationArea', StringType(), True),
+                StructField('Box', StringType(), True),
+                StructField('OriginalPriority', StringType(), True),
+                StructField('Priority', StringType(), True),
+                StructField('FinalPriority', IntegerType(), True),
+                StructField('ALSUnit', BooleanType(), True),
+                StructField('CallTypeGroup', StringType(), True),
+                StructField('NumAlarms', IntegerType(), True),
+                StructField('UnitType', StringType(), True),
+                StructField('UnitSequenceInCallDispatch', IntegerType(), True),
+                StructField('FirePreventionDistrict', StringType(), True),
+                StructField('SupervisorDistrict', StringType(), True),
+                StructField('Neighborhood', StringType(), True),
+                StructField('Location', StringType(), True),
+                StructField('RowID', StringType(), True),
+                StructField('Delay', FloatType(), True)])
+
+# Use the DataFrameReader interface to read a CSV file
+sf_fire_file = "./sf-fire-calls.csv"
+fire_df = spark.read.csv(sf_fire_file, header=True, schema=fire_schema)
+
+# Show the DataFrame  
+fire_df.show(5)  # Display the first 5 rows of the DataFrame
+
+# Print the schema of the DataFrame
+print(fire_df.printSchema())  # Print the schema in a tree format
+#print(fire_df.schema)  # Print the schema object definition
+
+# some basic transformations and actions
+few_fire_df = (fire_df
+               .select("IncidentNumber", "AvailableDtTm", "CallType")
+               .where(col("CallType") != "Medical Incident"))
+few_fire_df.show(5, truncate=False)  # Display the first 5 rows of the filtered DataFrame
+
+# return how many distinct CallTypes were recorded as the causes of the fire calls
+(fire_df
+    .select("CallType")
+    .where(col("CallType").isNotNull())  # Filter out null CallTypes
+    .agg(count_distinct("CallType").alias("DistinctCallTypes"))
+    .show()) # Display the count of distinct CallTypes
+
+# rename columns in the DataFrame
+new_fire_df = fire_df.withColumnRenamed("Delay", "ResponseDelayinMins")
+(new_fire_df
+    .select("IncidentNumber", "AvailableDtTm", "CallType", "ResponseDelayinMins")
+    .where(col("responseDelayinMins") > 5)
+    .show(5, truncate=False))
+
+fire_ts_df = (new_fire_df
+              .withColumn("IncidentDate", to_timestamp(col("CallDate"), "MM/dd/yyyy"))
+              .drop("CallDate")
+              .withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "MM/dd/yyyy"))
+              .drop("WatchDate")
+              .withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"), "MM/dd/yyyy hh:mm:ss a"))
+              .drop("AvailableDtTm"))
+
+(fire_ts_df
+    .select("IncidentDate", "OnWatchDate", "AvailableDtTS")
+    .show(5, truncate=False))
+
+# to save as Parquet file
+parquet_path = "./sf-few-fire-calls.parquet"
+if os.path.exists(parquet_path):
+    shutil.rmtree(parquet_path)  # Remove the existing directory if it exists
+few_fire_df.write.format("parquet").save(parquet_path)
+
+# alternatively, to save as a table in the Hive metastore
+parquet_table = "sf_few_fire_calls_parquet_table"
+if os.path.exists("spark-warehouse/" + parquet_table):
+    shutil.rmtree("spark-warehouse/" + parquet_table)
+few_fire_df.write.format("parquet").saveAsTable(parquet_table)
