6c3447e3ca07ffb0f19860829ce209db5dd24372
barondra <29912692+barondra@users.noreply.github.com>
2025-06-24 11:24:25 +0700

    finished chapter 4 book learningsparked2

--- SparkSQL5_Other_Formats.py
+++ SparkSQL5_Other_Formats.py
@@ -14,8 +14,8 @@ spark.catalog.setCurrentDatabase("learn_spark_db")
 json_dir = """./flights/summary-data/json/*"""
 try:
     json_df = spark.read.format("json").load(json_dir)
-    json_df.show()
     print("Succesfully load json file into DataFrame API")
+    json_df.show()
 except:
     raise Exception("Failed to read json file")
 
@@ -30,41 +30,49 @@ try:
         .option("nullValue", "") # Replace any null data field with quotes
         .load(csv_dir)
         )
-    csv_df.show()
     print("Succesfully load csv file into DataFrame API")
+    csv_df.show()
 except:
     raise Exception("Failed to read csv file")
 
 # Reading avro file into a DataFrame
 avro_dir = """./flights/summary-data/avro/*"""
-# avro_dir = "part-00000-tid-7128780539805330008-467d814d-6f80-4951-a951-f9f7fb8e3930-1434-1-c000.avro"
 try:
     avro_df = (spark.read.format("avro")
             .load(avro_dir)
             )
-    avro_df.show(truncate=False)
     print("Succesfully load avro file into DataFrame API")
+    avro_df.show(truncate=False)
 except:
     raise Exception("Failed to read avro file")
 
+# Reading orc file into a DataFrame
+orc_dir = """./flights/summary-data/orc/*"""
+try:
+    orc_df = (spark.read.format("orc")
+            .load(orc_dir)
+            )
+    print("Succesfully load orc file into DataFrame API")
+    orc_df.show(truncate=False)
+except:
+    raise Exception("Failed to read orc file")
+
 # Creating Spark SQL TABLE directly from JSON files (also possible for VIEW)
 try:
-    (spark.sql("""CREATE OR REPLACE TABLE flights_summary_tbl
+    (spark.sql("""CREATE TABLE IF NOT EXISTS flights_summary_tbl
            USING json
            OPTIONS (path "./flights/summary-data/json/*")
            """)
            )
     # and then see the VIEW
-    spark.table('flights_summary_tbl').show(5)
     print("Succesfully load json file into flights_summary_tbl")
-except Exception as e:
-    print("Failed to read json file")
-# finally:
-#     spark.createDataFrame(spark.catalog.listTables(), schema="`name` STRING, `catalog` STRING, `namespace` ARRAY<STRING>, `description` STRING, `tableType` STRING, `isTemporary` BOOLEAN").show()
+    spark.table('flights_summary_tbl').show(5)
+except:
+    raise Exception("Failed to read json file")
 
 # Creating Spark SQL TABLE directly from CSV files
 try:
-    (spark.sql("""CREATE OR REPLACE TABLE flights_summary_tbl
+    (spark.sql("""CREATE TABLE IF NOT EXISTS flights_summary_tbl
            USING csv
            OPTIONS (
                path "./flights/summary-data/csv/*",
@@ -75,46 +83,86 @@ try:
            """)
            )
     # and then see the TABLE
-    spark.table('flights_summary_tbl').show(5)
     print("Succesfully load csv file into flights_summary_tbl")
-except Exception as e:
-    print("Failed to read csv file")
-# finally:
-#     spark.createDataFrame(spark.catalog.listTables(), schema="`name` STRING, `catalog` STRING, `namespace` ARRAY<STRING>, `description` STRING, `tableType` STRING, `isTemporary` BOOLEAN").show()
+    spark.table('flights_summary_tbl').show(5)
+except:
+    raise Exception("Failed to read csv file")
 
 # Creating Spark SQL TEMP VIEW directly from avro files
 try:
-    (spark.sql("""CREATE OR REPLACE TEMP VIEW episode_view
+    (spark.sql("""CREATE OR REPLACE TEMP VIEW avro_episode_view
            USING avro
            OPTIONS (path "./flights/summary-data/avro/*")
            """)
            )
     # and then see the TABLE
-    spark.table('episode_view').show(5, truncate=False)
-    print("Succesfully load avro file into episode_view")
-except Exception as e:
-    print("Failed to read avro file")
+    print("Succesfully load avro file into avro_episode_view")
+    spark.table('avro_episode_view').show(5, truncate=False)
+except:
+    raise Exception("Failed to read avro file")
+
+# Creating Spark SQL TEMP VIEW directly from orc files
+try:
+    (spark.sql("""CREATE OR REPLACE TEMP VIEW orc_delay_flights_view
+           USING orc
+           OPTIONS (path "./flights/summary-data/orc/*")
+           """)
+           )
+    # and then see the TABLE
+    print("Succesfully load avro file into orc_delay_flights_view")
+    spark.table('orc_delay_flights_view').show(5, truncate=False)
+except:
+    raise Exception("Failed to read orc file")
 finally:
     spark.createDataFrame(spark.catalog.listTables(), schema="`name` STRING, `catalog` STRING, `namespace` ARRAY<STRING>, `description` STRING, `tableType` STRING, `isTemporary` BOOLEAN").show()
 
-# Writing DataFrames to JSON files
-(json_df.write.format("json")
-    .mode("overwrite")
-    .option("compression", "snappy")
-    .save("/tmp/data/json/df_json")
-    )
+try:
+    print("saving JSON, CSV, avro, and ORC files ... ")
+    # Writing DataFrames to JSON files
+    (json_df.write.format("json")
+        .mode("overwrite")
+        .option("compression", "snappy")
+        .save("/tmp/data/json/df_json")
+        )
 
-# Writing DataFrames to CSV files
-(csv_df.write.format("csv")
-    .mode("overwrite")
-    .save("/tmp/data/csv/df_csv")
-    )
+    # Writing DataFrames to CSV files
+    (csv_df.write.format("csv")
+        .mode("overwrite")
+        .save("/tmp/data/csv/df_csv")
+        )
 
-# Writing DataFrames to avro files
-(avro_df.write.format("avro")
-    .mode("overwrite")
-    .save("/tmp/data/avro/df_avro")
-    )
+    # Writing DataFrames to avro files
+    (avro_df.write.format("avro")
+        .mode("overwrite")
+        .save("/tmp/data/avro/df_avro")
+        )
+
+    # Writing DataFrames to orc files
+    (avro_df.write.format("orc")
+        .mode("overwrite")
+        .option("compression", "snappy")
+        .save("/tmp/data/orc/flights_orc")
+        )
+
+except:
+    raise Exception ("failed saving")
+finally:
+    print("Saving successful")
+
+# Furthermore, you can read image files and binary files, shown below as examples
+
+# from pyspark.ml import image
+# image_dir = "/databricks-datasets/learning-spark-v2/cctvVideos/train_images/"
+# images_df = spark.read.format("image").load(image_dir)
+# images_df.printSchema()
+# images.df.select("image.height", "image.width", "image.nChannels", "image.mode", "label").show(5, truncate=False)
+
+# binary_path = "/databricks-datasets/learning-spark-v2/cctvVideos/train_images/"
+# binary_files_df = (spark.read.format("binaryFile")
+#                    .option("pathGlobFilter", "*.jpg")
+#                    .load(binary_path))
+# binary_files_df.show(5)
+
 
 # end the session
 spark.stop()
